008350d79cfaa3f1a621c5fa8baa8451cdd8c20f
haoyun_tw <haoyun.tw@gmail.com>
2019-11-20 19:04:17 +0800

    Use google-java-format-diff.py to only compare diff
    Return error code when any error

--- format-code.sh
+++ format-code.sh
@@ -8,15 +8,25 @@ then
 fi
 if [ ! -f google-java-format-diff.py ]
 then
-    curl -LJ0 "https://github.com/google/google-java-format/blob/master/scripts/google-java-format-diff.py"
+    curl -LJO "https://raw.githubusercontent.com/google/google-java-format/master/scripts/google-java-format-diff.py"
     chmod 755 google-java-format-diff.py
 fi
 cd ..
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
-for FILE in $changed_java_files
+for FILE in ${changed_java_files}
 do
-  git diff -U0 $FILE | .cache/google-java-format-diff.py -p1 -a --google-java-format .cache/google-java-format-1.7-all-deps.jar
+  echo ============================================================
+  echo $FILE
+  DIFF_OUTPUT=$(git diff --cached -U0 $FILE | .cache/google-java-format-diff.py -p1 -a --skip-sorting-imports --google-java-format .cache/google-java-format-1.7-all-deps.jar)
+  echo "${DIFF_OUTPUT}"
+  if [ ! -z "$DIFF_OUTPUT" ]
+  then
+    RETURN_CODE=1
+  fi
+
 done
+
+exit $RETURN_CODE
 
