b19f868b244c343b8873cd9f9971a0168409dbdd
Kenny Yin <haoyun.tw@gmail.com>
2024-06-22 14:20:50 +0800

    shellcheck fix

--- format-code.sh
+++ format-code.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 mkdir -p .cache
-cd .cache
+(
+cd .cache || exit 1
 if [ ! -f google-java-format-1.22.0-all-deps.jar ]
 then
     curl -LJO "https://github.com/google/google-java-format/releases/download/v1.22.0/google-java-format-1.22.0-all-deps.jar"
@@ -11,17 +12,17 @@ then
     curl -LJO "https://raw.githubusercontent.com/google/google-java-format/v1.22.0/scripts/google-java-format-diff.py"
     chmod 755 google-java-format-diff.py
 fi
-cd ..
+)
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo $changed_java_files
+echo "$changed_java_files"
 for FILE in ${changed_java_files}
 do
   echo ============================================================
-  echo $FILE
-  DIFF_OUTPUT=$(git diff --cached -U0 $FILE | .cache/google-java-format-diff.py -p1 -a --skip-sorting-imports --google-java-format .cache/google-java-format-1.7-all-deps.jar)
+  echo "$FILE"
+  DIFF_OUTPUT=$(git diff --cached -U0 "$FILE" | .cache/google-java-format-diff.py -p1 -a --skip-sorting-imports --google-java-format .cache/google-java-format-1.7-all-deps.jar)
   echo "${DIFF_OUTPUT}"
-  if [ ! -z "$DIFF_OUTPUT" ]
+  if [ -n "$DIFF_OUTPUT" ]
   then
     RETURN_CODE=1
   fi
