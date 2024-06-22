1c84ad7d44b2238aa94188e74aa9e92ea5139274
HY Yin <haoyun.tw@gmail.com>
2024-06-22 13:58:37 +0800

    Update google-java-format-1.22.0-all-deps.jar

--- format-code.sh
+++ format-code.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.7-all-deps.jar ]
+if [ ! -f google-java-format-1.22.0-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.7/google-java-format-1.7-all-deps.jar"
+    curl -LJO "https://github.com/google/google-java-format/releases/download/v1.22.0/google-java-format-1.22.0-all-deps.jar"
     chmod 755 google-java-format-1.7-all-deps.jar
 fi
 if [ ! -f google-java-format-diff.py ]
