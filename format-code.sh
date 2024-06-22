b11355b8bc6f2b2c50b9b2f545be61f6bbbf50bd
Kenny Yin <haoyun.tw@gmail.com>
2024-06-22 14:32:51 +0800

    Add GOOGLE_JAVA_FORMAT_VERSION and exit 1 form subshell

--- format-code.sh
+++ format-code.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env sh
+GOOGLE_JAVA_FORMAT_VERSION=1.22.0
 mkdir -p .cache
 (
 cd .cache || exit 1
-if [ ! -f google-java-format-1.22.0-all-deps.jar ]
+if [ ! -f google-java-format-"$GOOGLE_JAVA_FORMAT_VERSION"-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/v1.22.0/google-java-format-1.22.0-all-deps.jar"
-    chmod 755 google-java-format-1.7-all-deps.jar
+  curl -LJO "https://github.com/google/google-java-format/releases/download/v$GOOGLE_JAVA_FORMAT_VERSION/google-java-format-$GOOGLE_JAVA_FORMAT_VERSION-all-deps.jar"
+  chmod 755 google-java-format-1.7-all-deps.jar
 fi
 if [ ! -f google-java-format-diff.py ]
 then
-    curl -LJO "https://raw.githubusercontent.com/google/google-java-format/v1.22.0/scripts/google-java-format-diff.py"
-    chmod 755 google-java-format-diff.py
+  curl -LJO "https://raw.githubusercontent.com/google/google-java-format/v$GOOGLE_JAVA_FORMAT_VERSION/scripts/google-java-format-diff.py"
+  chmod 755 google-java-format-diff.py
 fi
-)
+) || exit 1
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo "$changed_java_files"
@@ -20,13 +21,12 @@ for FILE in ${changed_java_files}
 do
   echo ============================================================
   echo "$FILE"
-  DIFF_OUTPUT=$(git diff --cached -U0 "$FILE" | .cache/google-java-format-diff.py -p1 -a --skip-sorting-imports --google-java-format .cache/google-java-format-1.7-all-deps.jar)
+  DIFF_OUTPUT=$(git diff --cached -U0 "$FILE" | .cache/google-java-format-diff.py -p1 -a --skip-sorting-imports --google-java-format .cache/google-java-format-"$GOOGLE_JAVA_FORMAT_VERSION"-all-deps.jar)
   echo "${DIFF_OUTPUT}"
   if [ -n "$DIFF_OUTPUT" ]
   then
     RETURN_CODE=1
   fi
-
 done
 
 exit $RETURN_CODE
